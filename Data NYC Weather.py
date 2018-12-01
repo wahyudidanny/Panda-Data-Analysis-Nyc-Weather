b67283cbdd37efbb3d91ccd31b193493c0af714d
danny wahyudi <wahyudidanny23@gmail.com>
2018-12-01 22:23:42 +0700

    Data Analysis NYC Weather
    Data Weather NYC

---
+++ Data NYC Weather.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+df = pd.read_csv('E:\\Github Repository\\Panda-Data-Analysis-Nyc-Weather\\nyc_weather_2016.csv')
+df
+
+
+# In[6]:
+
+
+df['maximum temperature'].max()
+
+
+# In[ ]:
+
+
+
+
